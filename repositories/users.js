ac964e512c74fc2b01745198aca54cbdc7fd5b96
beata <beata@emc2wave.com>
2020-03-24 15:58:10 -0700

    add fs, fs.accessSync()

--- repositories/users.js
+++ repositories/users.js
@@ -1,8 +1,13 @@
+const fs = require('fs');
+
 class UsersRepository {
   constructor(filename) {
     if (!filename) {
       throw new Error('Creating a repository requires a filename');
     }
+
+    this.filename = filename;
+    fs.accessSync(this.filename);
   }
 }
 
