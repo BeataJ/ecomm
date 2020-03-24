11149a23269c2e919232a94acc16699828a9ac62
beata <beata@emc2wave.com>
2020-03-24 16:32:51 -0700

    add  async getAll() with fs.promises.readFile

--- repositories/users.js
+++ repositories/users.js
@@ -13,6 +13,19 @@ class UsersRepository {
       fs.writeFileSync(this.filename, '[]');
     }
   }
+
+  async getAll() {
+    // Open the file called this.filename
+    const contents = await fs.promises.readFile(this.filename, {
+      encoding: 'utf8'
+    });
+
+    // Read its contents
+
+    // parse the contents
+
+    // Return the parsed data
+  }
 }
 
 new UsersRepository('users.json');
