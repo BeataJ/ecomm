ddc0922f59299ce29954e406c6ee2e453e8d7776
beata <beata@emc2wave.com>
2020-03-25 14:25:55 -0700

    add getOne() , delete()

--- repositories/users.js
+++ repositories/users.js
@@ -43,16 +43,31 @@ class UsersRepository {
   randomId() {
     return crypto.randomBytes(4).toString('hex');
   }
+
+  async getOne(id) {
+    const records = await this.getAll();
+    return records.find(record => record.id === id);
+  }
+
+  async delete(id) {
+    const records = await this.getAll();
+    const filteredRecords = records.filter(record => record.id !== id);
+    await this.writeAll(filteredRecords);
+  }
 }
 
 const test = async () => {
   const repo = new UsersRepository('users.json');
 
-  await repo.create({ email: 'test@test.com', password: 'password' });
+  // const user = await repo.getOne('1bd3bf24');
 
-  const users = await repo.getAll();
+  await repo.delete('1bd3bf24');
 
-  console.log(users);
+  // await repo.create({ email: 'test@test.com', password: 'password' });
+
+  // const users = await repo.getAll();
+
+  // console.log(user);
 };
 
 test();
