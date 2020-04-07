65e40263d6d7c90ff1d9fc17efb6695c12692d3b
beata <beata@emc2wave.com>
2020-04-07 15:13:31 -0700

    add /

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -4,9 +4,9 @@ const productsNewTemplate = require('../../views/admin/products/new');
 
 const router = express.Router();
 
-router.get('admin/products', (req, res) => {});
+router.get('/admin/products', (req, res) => {});
 
-router.get('admin/products/new', (req, res) => {
+router.get('/admin/products/new', (req, res) => {
   res.send(productsNewTemplate({}));
 });
 
