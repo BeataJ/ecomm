c51ff18d8af92d8a58b8cbf4f6e3ca2fe098a389
beata <beata@emc2wave.com>
2020-04-07 17:21:41 -0700

    create router.post for new with validationResults

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
+const { validationResult } = require('express-validator');
+
 const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
 
-router.get('/admin/products', [requireTitle, requirePrice], (req, res) => {});
+router.get('/admin/products', (req, res) => {});
 
 router.get('/admin/products/new', (req, res) => {
   res.send(productsNewTemplate({}));
+});
+
+router.post('/admin/products/new', [requireTitle, requirePrice], (res, req) => {
+  const errors = validationResult(req);
+  console.log(errors);
+
+  res.send('submitted');
 });
 
 module.exports = router;
