87c7d3c1e23b3d4cd0bc3a30c5543af01a45aa51
beata <beata@emc2wave.com>
2020-04-07 17:31:32 -0700

    correct name form

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const { validationResult } = require('express-validator');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
-const { validationResult } = require('express-validator');
 
 const { requireTitle, requirePrice } = require('./validators');
 
@@ -13,7 +13,7 @@ router.get('/admin/products/new', (req, res) => {
   res.send(productsNewTemplate({}));
 });
 
-router.post('/admin/products/new', [requireTitle, requirePrice], (res, req) => {
+router.post('/admin/products/new', [requireTitle, requirePrice], (req, res) => {
   const errors = validationResult(req);
   console.log(errors);
 
