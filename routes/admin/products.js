4a9bac83b2176cd3d452f843b95ca5a60d3783fb
beata <beata@emc2wave.com>
2020-04-07 17:13:28 -0700

    add validators to products

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
+const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
 
-router.get('/admin/products', (req, res) => {});
+router.get('/admin/products', [requireTitle, requirePrice], (req, res) => {});
 
 router.get('/admin/products/new', (req, res) => {
   res.send(productsNewTemplate({}));
