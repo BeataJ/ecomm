3bdff6f990274490e0b66876bb24a5c168965146
beata <beata@emc2wave.com>
2020-04-11 22:34:13 -0700

    add code to router post :id arrow func

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -51,7 +51,21 @@ router.post(
   upload.single('image'),
   [requireTitle, requirePrice],
   handleErrors(productsEditTemplate),
-  async (req, res) => {}
+  async (req, res) => {
+    const changes = req.body;
+
+    if (req.file) {
+      changes.image = req.file.buffer.toString('base64');
+    }
+
+    try {
+      await productsRepo.update(req.params.id, changes);
+    } catch (err) {
+      return res.send('Could not find item');
+    }
+
+    res.redirect('/admin/products');
+  }
 );
 
 module.exports = router;
