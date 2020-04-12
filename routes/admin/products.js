9f4a2b22d2d5278063c750c632d4465348cfeb22
beata <beata@emc2wave.com>
2020-04-11 22:20:20 -0700

    add middlewares

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -45,6 +45,13 @@ router.get('/admin/products/:id/edit', requireAuth, async (req, res) => {
   res.send(productsEditTemplate({ product }));
 });
 
-router.post('/admin/products/:id/edit', requireAuth, (req, res) => {});
+router.post(
+  '/admin/products/:id/edit',
+  requireAuth,
+  upload.single('image'),
+  [requireTitle, requirePrice],
+  handleErrors(productsEditTemplate),
+  async (req, res) => {}
+);
 
 module.exports = router;
