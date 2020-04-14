2d6be1d4e8b42da786bd8c95ac0149e08586c45b
beata <beata@emc2wave.com>
2020-04-14 14:25:48 -0700

    take cart from cartsRepo and loop

--- routes/carts.js
+++ routes/carts.js
@@ -28,9 +28,14 @@ router.post('/cart/products', async (req, res) => {
   res.send('Product added to cart');
 });
 
-router.get('cart', (req, res) => {
+router.get('cart', async (req, res) => {
   if (!req.session.cartId) {
     return res.redirect('/');
+  }
+
+  const cart = await cartsRepo.getOne(req.session.cartId);
+
+  for (let item of cart.items) {
   }
 });
 
