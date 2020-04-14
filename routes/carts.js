83a9f510838f2651fbc3cf7d33510a8b7de80a47
beata <beata@emc2wave.com>
2020-04-14 16:47:13 -0700

    add post handler for /cart/products/delete

--- routes/carts.js
+++ routes/carts.js
@@ -46,4 +46,8 @@ router.get('/cart', async (req, res) => {
   res.send(cartShowTemplate({ items: cart.items }));
 });
 
+router.post('/cart/products/delete', async (req, res) => {
+  console.log(req.body.itemId);
+});
+
 module.exports = router;
